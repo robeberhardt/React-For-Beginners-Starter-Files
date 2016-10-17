7dceabb29dea2ba0fb7f7d5e0fc9b9c1ea691791
Rob Eberhardt <rob.eberhardt@gmail.com>
2016-10-17 16:53:06 -0400

    transition route

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -10,22 +10,27 @@ class StorePicker extends React.Component {
   goToStore(event) {
     event.preventDefault();
     // first grab the text from the input box
-    console.log('you changed the url', this.storeInput.value);
-
+    const storeId = this.storeInput.value;
+    console.log(`going to ${storeId}`);
+    
     // now transition to the correct page
-
+    this.context.router.transitionTo(`/store/${storeId}`);
   }
 
   render() {
     return (
       <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
         { /* Look here */ }
-        <h2>Please Enter A Store</h2>
+        <h2>Please Enter A Blarg</h2>
         <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
         <button type="submit">Visit Store →</button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
