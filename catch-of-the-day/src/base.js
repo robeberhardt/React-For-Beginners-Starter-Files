c8e51581911a5c1d315cfb97e15adea6c92a965a
Rob Eberhardt <rob.eberhardt@gmail.com>
2016-10-18 12:23:21 -0400

    save fishes in firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDgSBJ1Uf84_9TtEmWiAn2w-VJ8eMVxcbE",
+  authDomain: "catch-of-the-day-26b6e.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-26b6e.firebaseio.com",
+});
+
+export default base;
